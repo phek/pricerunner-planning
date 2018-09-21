d1582fab2ea6a8ab65b81c8a6a56e6d1209acd3f
Philip Ekblom <philip.ekblom@gmail.com>
2018-09-21 11:27:53 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './components/appContainer/AppContainer';
+import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <AppContainer />
+    </ApolloProvider>,
+    document.getElementById('root'));
+registerServiceWorker();
